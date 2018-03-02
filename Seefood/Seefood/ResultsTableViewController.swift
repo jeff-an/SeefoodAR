e84506e9d1417d01554d93c2ec11468efe00c441
sarayd <saradubusiness@gmail.com>
2018-03-01 16:46:21 -0800

    static data

--- Seefood/Seefood/ResultsTableViewController.swift
+++ Seefood/Seefood/ResultsTableViewController.swift
@@ -11,29 +11,78 @@ import UIKit
 class ResultsTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var currCount = 0
     var selectedRow = 0
-    var results: [DishResult] = []
+    //var results: [DishResult] = []
     let dishData = [
         [
-            "imageId": "0",
-            "title": "Salmon Bento Box",
+            "imageId": "salmonNigiri",
+            "title": "Salmon Nigiri",
             "restaurant": "Sushi Moto",
             "price": "12.99",
             "descripLabel": "Food"
         ],
         [
-            "imageId": "0",
-            "title": "Salmon Bento Box",
+            "imageId": "tunaNigiri",
+            "title": "Tuna Nigiri",
             "restaurant": "Sushi Moto",
             "price": "12.99",
             "descripLabel": "Food"
         ],
         [
-            "imageId": "0",
-            "title": "Salmon Bento Box",
+            "imageId": "dumpling",
+            "title": "Chicken Dumpling",
             "restaurant": "Sushi Moto",
             "price": "12.99",
             "descripLabel": "Food"
         ],
+        [
+            "imageId": "californiaRoll",
+            "title": "California Roll",
+            "restaurant": "Sushi Moto",
+            "price": "12.99",
+            "descripLabel": "Food"
+        ],
+        [
+            "imageId": "spicyTunaRoll",
+            "title": "Spicy Tuna Roll",
+            "restaurant": "Sushi Moto",
+            "price": "12.99",
+            "descripLabel": "Food"
+        ],
+        [
+            "imageId": "unagi",
+            "title": "Unagi",
+            "restaurant": "Sushi Moto",
+            "price": "12.99",
+            "descripLabel": "Food"
+        ],
+        [
+            "imageId": "yellowtailNigiri",
+            "title": "Yellow Tail Nigiri",
+            "restaurant": "Sushi Moto",
+            "price": "12.99",
+            "descripLabel": "Food"
+        ],
+        [
+            "imageId": "shrimpNigiri",
+            "title": "Shrimp Nigiri",
+            "restaurant": "Sushi Moto",
+            "price": "12.99",
+            "descripLabel": "Food"
+        ],
+        [
+            "imageId": "tamago",
+            "title": "Tamago",
+            "restaurant": "Sushi Moto",
+            "price": "12.99",
+            "descripLabel": "Food"
+        ],
+        [
+            "imageId": "cookie",
+            "title": "Chocolate Chip Cookies",
+            "restaurant": "Sushi Moto",
+            "price": "12.99",
+            "descripLabel": "Food"
+        ]
     ]
     
 
@@ -48,7 +97,7 @@ class ResultsTableViewController: UIViewController, UITableViewDelegate, UITable
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return currCount + 1
+       return dishData.count + 1
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -66,24 +115,26 @@ class ResultsTableViewController: UIViewController, UITableViewDelegate, UITable
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let titleCell: TitleCell = tableView.dequeueReusableCell(withIdentifier: "titlecell") as! TitleCell
+        let cell : ResultsTableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell") as! ResultsTableViewCell
+        cell.selectionStyle = .none
+        
         if(indexPath.row == 0){
             titleCell.isUserInteractionEnabled = false
             return titleCell
+        }else{
+            let dish = dishData[indexPath.row - 1]
+            if indexPath.row > 0 {
+                //put in custom titles and descrips
+                cell.foodImage.image = UIImage(named: dish["imageId"]!)
+                cell.title.text = dish["title"]!
+                cell.price.text = dish["price"]!
+                cell.restaurant.text = dish["restaurant"]!
+                cell.descripLabel.text = dish["descripLabel"]!
+            }
+            cell.alpha = 0
+            UIView.animate(withDuration: 0.75, animations: { cell.alpha = 1 })
+            return cell
         }
-        let cell : ResultsTableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell") as! ResultsTableViewCell
-        cell.selectionStyle = .none
-        let dish = dishData[indexPath.row]
-        if indexPath.row > 0 {
-           //put in custom titles and descrips
-            cell.foodImage.image = UIImage(named: dish["imageId"]!)
-            cell.title.text = dish["title"]!
-            cell.price.text = dish["price"]!
-            cell.restaurant.text = dish["restaurant"]!
-            cell.descripLabel.text = dish["descripLabel"]!
-        }
-        cell.alpha = 0
-        UIView.animate(withDuration: 0.75, animations: { cell.alpha = 1 })
-        return cell
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
---
+++ Seefood/Seefood/Variables.swift
@@ -0,0 +1,9 @@
+//
+//  Variables.swift
+//  Seefood
+//
+//  Created by Sara on 3/1/18.
+//  Copyright © 2018 Duvelop. All rights reserved.
+//
+
+import Foundation
