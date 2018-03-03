a054a1c046cb4facdf9bb7f2cff1892e0f81c936
sarayd <saradubusiness@gmail.com>
2018-03-03 07:49:45 -0800

    table view

---
+++ Seefood/Seefood/ResultsTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ResultsTableViewCell.swift
+//  Seefood
+//
+//  Created by Sara on 3/3/18.
+//  Copyright © 2018 Duvelop. All rights reserved.
+//
+
+import UIKit
+class ResultsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var foodImage: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var descripLabel: UILabel!
+    
+}
