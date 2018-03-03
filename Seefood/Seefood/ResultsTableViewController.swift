e036ede26f50b92c882bf6e7ab0727b161a79de0
sarayd <saradubusiness@gmail.com>
2018-03-03 07:49:45 -0800

    table view

---
+++ Seefood/Seefood/ResultsTableViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ResultsTableViewController.swift
+//  Seefood
+//
+//  Created by Sara on 3/3/18.
+//  Copyright © 2018 Duvelop. All rights reserved.
+//
+
+import UIKit
+
+class ResultsTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var currCount = 0
+    var selectedRow = 0
+    
+    @IBOutlet weak var resultsTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.resultsTableView.register(UINib(nibName: "ResultsTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+        self.resultsTableView.register(UINib(nibName: "TitleCell", bundle: nil), forCellReuseIdentifier: "titlecell")
+        self.resultsTableView.separatorStyle = .none
+        
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return currCount + 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if(indexPath.row == 0){
+            return 60
+        }else{
+            return 140
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedRow = indexPath.row
+        self.performSegue(withIdentifier: "next", sender: self)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : ResultsTableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell") as! ResultsTableViewCell
+        cell.selectionStyle = .none
+        
+        let titleCell: TitleCell = tableView.dequeueReusableCell(withIdentifier: "titlecell") as! TitleCell
+        if(indexPath.row == 0){
+            titleCell.isUserInteractionEnabled = false
+            return titleCell
+        }
+        
+        if indexPath.row > 0 {
+           //put in custom titles and descrips
+            
+        }
+        cell.alpha = 0
+        UIView.animate(withDuration: 0.75, animations: { cell.alpha = 1 })
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
---
+++ Seefood/Seefood/TitleCell.swift
@@ -0,0 +1,14 @@
+//
+//  TitleCell.swift
+//  Seefood
+//
+//  Created by Sara on 3/3/18.
+//  Copyright © 2018 Duvelop. All rights reserved.
+//
+
+import UIKit
+
+class TitleCell: UITableViewCell {
+    
+}
+
