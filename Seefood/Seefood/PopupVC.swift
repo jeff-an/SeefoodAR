3d563495b0d14513e28780689643e095199c8fc6
Jeff <jeff.an@edu.uwaterloo.ca>
2018-03-03 18:18:21 -0500

    Create popup modal for search by restaurant

---
+++ Seefood/Seefood/PopupVC.swift
@@ -0,0 +1,21 @@
+//
+//  PopupVC.swift
+//  Seefood
+//
+//  Created by Jeff on 2018-03-03.
+//  Copyright © 2018 Duvelop. All rights reserved.
+//
+
+import UIKit
+
+class PopupVC: UIViewController {
+    @IBOutlet weak var TextAction: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
