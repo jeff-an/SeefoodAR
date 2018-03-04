723801b10e835fc9b10159af8ed0a5749cc014e0
Eric Zhao <ericlzhao28@gmail.com>
2018-03-04 04:32:01 -0800

    Yay everything works

--- SeeFoodChat/src/main/main.py
+++ SeeFoodChat/src/main/main.py
@@ -3,31 +3,76 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
 
+genres = {
+    "japanese": ["asian", "japan", "japanese", "sushi"],
+    "korean": ["asian", "korean", "korea"],
+    "chinese": ["asian", "chinese", "china"],
+    "american": ["american", "america", "burger", "mcdonalds"],
+    "comfort": ["comfort", "cheap", "fast"],
+    "indian": ["spicy", "indian", "india", "curry"],
+}
+portions = {
+    "small": ["small", "little", "bit"],
+    "medium": ["medium", "enough", "middle"],
+    "large": ["large", "lot"],
+}
+misc = {
+    "spicy": ["spicy", "hot"],
+    "organic": ["organic", "natural"],
+    "glutenfree": ["gluten free", "gluten-free"],
+    "vegetarian": ["vegetarian", "vegan"],
+    "no": ["nope", "no", "fuck off", "bye", "thanks", "stop", "done"],
+}
+
+for key, samples in genres.items():
+  vecs = []
+  for sample in samples:
+    vecs += [embed(x) for x in sample.split(" ")]
+  genres[key] = np.mean(vecs, axis=0)
+for key, samples in portions.items():
+  vecs = []
+  for sample in samples:
+    vecs += [embed(x) for x in sample.split(" ")]
+  portions[key] = np.mean(vecs, axis=0)
+for key, samples in misc.items():
+  vecs = []
+  for sample in samples:
+    vecs += [embed(x) for x in sample.split(" ")]
+  misc[key] = np.mean(vecs, axis=0)
+
+
 def handle(query, type):
   type = str(type).strip()
-  query = str(query).strip()
-
+  query = str(query).lower().strip()
+  query_vec = np.mean([embed(x) for x in query.split(" ")], axis=0)
   if type == "genre":
-    return handle_genres(query)
+    return handle_genres(query_vec)
   elif type == "portion":
-    return handle_portion(query)
+    return handle_portion(query_vec)
   else:
-    return handle_general(query)
+    return handle_general(query_vec)
 
-def handle_genres(query):
-  genres = {}
-  for n in ["chinese", "korean", "american", "sushi", "mexican"]:
-    genres[n] = cosine_similarity([np.mean([embed(x) for x in query.split(" ")], axis=0)], [embed(n)])[0][0]
-  print(sorted(genres.items(), key=lambda x: x[1]))
-  return sorted(genres.items(), key=lambda x: x[1])[-1][0]
 
-def handle_portion(query):
-  genres = {}
-  for n in ["large", "small", "medium"]:
-    genres[n] = cosine_similarity([np.mean([embed(x) for x in query.split(" ")], axis=0)], [embed(n)])[0][0]
-  print(sorted(genres.items(), key=lambda x: x[1]))
-  return sorted(genres.items(), key=lambda x: x[1])[-1][0]
-  
-def handle_general(query):
-  return "Sure"
+def handle_genres(query_vec):
+  results = {}
+  for key, vec in genres.items():
+    results[key] = cosine_similarity([query_vec], [vec])[0][0]
+  print(sorted(results.items(), key=lambda x: x[1]))
+  return sorted(results.items(), key=lambda x: x[1])[-1][0]
+
+
+def handle_portion(query_vec):
+  results = {}
+  for key, vec in portions.items():
+    results[key] = cosine_similarity([query_vec], [vec])[0][0]
+  print(sorted(results.items(), key=lambda x: x[1]))
+  return sorted(results.items(), key=lambda x: x[1])[-1][0]
+
+
+def handle_general(query_vec):
+  results = {}
+  for key, vec in misc.items():
+    results[key] = cosine_similarity([query_vec], [vec])[0][0]
+  print(sorted(results.items(), key=lambda x: x[1]))
+  return sorted(results.items(), key=lambda x: x[1])[-1][0]
 
